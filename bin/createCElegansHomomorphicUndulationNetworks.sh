803dc706bb8ffeaf31d4a04b7dcbe2c409dbd3c9
Tom Portegys <portegys@gmail.com>
2014-05-05 23:35:45 -0700

    Update scripts

---
+++ bin/createCElegansHomomorphicUndulationNetworks.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]
+then
+./bionet -createHomomorphicNetworks -undulationMovements 25 -loadNetwork CElegans_network.txt \
+-synapseWeights 0.0 .25 .1 -populationSize 500 -numOffspring 250 -numGenerations 250 \
+-crossoverRate .9 -mutationRate .05 -synapseCrossoverBondStrength .5 \
+-synapseOptimizedPathLength 4 -saveMorph morph.txt -numThreads 2
+else
+./bionet -createHomomorphicNetworks -undulationMovements 25 -loadNetwork CElegans_network.txt \
+-synapseWeights 0.0 .25 .1 -populationSize 500 -numOffspring 250 -numGenerations 250 \
+-crossoverRate .9 -mutationRate .05 -synapseCrossoverBondStrength .5 \
+-synapseOptimizedPathLength 4 -saveMorph morph.txt -randomSeed ${@} -numThreads 2
+fi
