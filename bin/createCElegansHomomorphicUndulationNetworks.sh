429a904df87dd00b5becb8acec52b68c0b15c2e6
Tom Portegys <portegys@gmail.com>
2014-05-07 23:51:38 -0700

    Fix usage

--- bin/createCElegansHomomorphicUndulationNetworks.sh
+++ bin/createCElegansHomomorphicUndulationNetworks.sh
@@ -3,10 +3,10 @@ then
 ./bionet -createHomomorphicNetworks -undulationMovements 25 -loadNetwork CElegans_network.txt \
 -synapseWeights 0.0 .25 .1 -populationSize 500 -numOffspring 250 -numGenerations 250 \
 -crossoverRate .9 -mutationRate .05 -synapseCrossoverBondStrength .5 \
--synapseOptimizedPathLength 4 -saveMorph morph.txt -numThreads 2
+-synapseOptimizedPathLength 4 -saveMorph morph0.txt -numThreads 2
 else
 ./bionet -createHomomorphicNetworks -undulationMovements 25 -loadNetwork CElegans_network.txt \
 -synapseWeights 0.0 .25 .1 -populationSize 500 -numOffspring 250 -numGenerations 250 \
 -crossoverRate .9 -mutationRate .05 -synapseCrossoverBondStrength .5 \
--synapseOptimizedPathLength 4 -saveMorph morph.txt -randomSeed ${@} -numThreads 2
+-synapseOptimizedPathLength 4 -saveMorph morph0.txt -randomSeed ${@} -numThreads 2
 fi
--- bin/createCElegansHomomorphicUndulationNetworksResume.sh
+++ bin/createCElegansHomomorphicUndulationNetworksResume.sh
@@ -1,12 +1,12 @@
 if [ $# -eq 0 ]
 then
-./bionet -createHomomorphicNetworks -undulationMovements 20 -loadMorph morph0.txt \
--numGenerations 1000 \
+./bionet -createHomomorphicNetworks -undulationMovements 25 -loadMorph morph0.txt \
+-numGenerations 250 \
 -crossoverRate .9 -mutationRate .05 -synapseCrossoverBondStrength .5 \
 -synapseOptimizedPathLength 4 -saveMorph morph1.txt -numThreads 2
 else
-./bionet -createHomomorphicNetworks -undulationMovements 20 -loadMorph morph0.txt \
--numGenerations 1000 \
+./bionet -createHomomorphicNetworks -undulationMovements 25 -loadMorph morph0.txt \
+-numGenerations 250 \
 -crossoverRate .9 -mutationRate .05 -synapseCrossoverBondStrength .5 \
 -synapseOptimizedPathLength 4 -saveMorph morph1.txt -randomSeed ${@} -numThreads 2
 fi
